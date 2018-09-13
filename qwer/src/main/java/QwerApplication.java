96e7bdb6571e863657776d61e3c9fe00e1f4aa93
KirillSakolin <kirillsakolin@coherentsolutions.com>
2018-09-13 16:31:37 +0300

    new module

---
+++ qwer/src/main/java/QwerApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwerApplication {
+
+
+	/*
+	test2
+	*/
+	public static void main(String[] args) {
+		SpringApplication.run(QwerApplication.class, args);
+	}
+}
